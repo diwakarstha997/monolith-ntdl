84e96601cc0bc933da26c994f7010382ed115c7d
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-02-23 10:58:30 +1100

    Bug Fix

--- client/src/axios/taskAxios.js
+++ client/src/axios/taskAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // This file sends api request for task resource
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = import.meta.env.PROD? "https://monolith-ntdl.onrender.com" : import.meta.env.VITE_API_BASE_URL
 const TASK_ENPOINT = '/api/tasks'
 
 // FRONTEND CRUD OPERATIONS
